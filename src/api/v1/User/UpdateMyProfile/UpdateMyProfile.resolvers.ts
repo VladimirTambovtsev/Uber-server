c3f61fa8173dee44a2a92e642fc56506c6d786f8
Vladimir Tambovtsev <tambovcev99@mail.ru>
2020-02-18 14:00:50 +0300

    set up updateProfile resolver & schema

---
+++ src/api/v1/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -0,0 +1,39 @@
+import {
+  UpdateMyProfileMutationArgs,
+  UpdateMyProfileResponse
+} from './../../../../types/graph.d'
+import { Resolvers } from '../../../../types/resolvers'
+import privateResolver from '../../../../utils/privateResolver'
+import User from '../../../../entities/User'
+
+const resolvers: Resolvers = {
+  Mutation: {
+    UpdateMyProfile: privateResolver(
+      async (
+        _,
+        args: UpdateMyProfileMutationArgs,
+        { req }
+      ): Promise<UpdateMyProfileResponse> => {
+        const user: User = req.user
+        const notNull = {}
+        Object.keys(args).forEach(key => {
+          if (args[key] !== null) {
+            notNull[key] = args[key]
+          }
+        })
+        try {
+          if (args.password !== null) {
+            user.password = args.password
+            user.save()
+          }
+          await User.update({ id: user.id }, { ...notNull })
+          return { ok: true, error: null }
+        } catch (error) {
+          return { ok: false, error: error.message }
+        }
+      }
+    )
+  }
+}
+
+export default resolvers
